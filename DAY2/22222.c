0b4197c9ffd371630857c84a9c402746391bda13
ChanWooong <104729817+ChanWooong@users.noreply.github.com>
2023-05-19 20:57:24 +0900

    Update 22222.c

--- DAY2/22222.c
+++ DAY2/22222.c
@@ -10,11 +10,12 @@ int main()
 
 	scanf("%s", ch); // 1
 	
-    ptoken = strtok(ch, delimiter);
+    ptoken = strtok(ch, delimiter);		 // delimiter의 기본형이 char*임. 포인터로 접근하는 만큼 &를 붙여 주소값으로 지정해주어야 함. 
 
-	for (i = strlen(*ptoken)-1; i >= 0; i--) // 2
+	for (i = strlen(*ptoken)-1; i >= 0; i--) // ptoken에 *을 한번더 달면 기존에도 char* ptoken이었는데, 
+						 //char** ptoken 형식이 되어서 엄밀히는 string 배열의 주소가 됨.
 	{
-        printf("%c", *(ptoken + i));
+        printf("%c", *(ptoken + i));		 // ptoken + i를 통해 원하는 인덱스의 배열값을 잡아주고 *을 통해서 배열 내부의 값을 접근함.
 	}
 }
 
